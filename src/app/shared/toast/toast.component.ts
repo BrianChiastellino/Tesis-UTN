5af180b4851d75f8ed83de0cc992b8736ebe7e21
Brian Chaste <b.chiastellino@gmail.com>
2024-06-24 16:54:40 -0300

    toast add messages

--- src/app/shared/toast/toast.component.ts
+++ src/app/shared/toast/toast.component.ts
@@ -10,4 +10,26 @@ export class ToastComponent {
 
   //todo: terminar toast, hacer eficiente, crear mensajes en ts y pasarlos al html
 
+  // idea : crear un enum para ponerlo como key es decir, Toast.error, emitir un valor mediante el padre, 
+
+  private messages: { [key: string]: { title: string, body: string } } = {
+    success: {
+      title: 'Éxito!',
+      body: '¡Operación realizada con éxito!'
+    },
+    error: {
+      title: 'Error',
+      body: 'Ha ocurrido un error. Por favor, inténtelo de nuevo.'
+    },
+    warning: {
+      title: 'Advertencia',
+      body: 'Algo no está del todo bien. Tenga precaución.'
+    }
+  };
+
+  constructor() {
+    console.log( this.messages['error'].body )
+  }
+
+
 }
